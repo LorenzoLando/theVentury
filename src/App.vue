26efdbfa61feb5639174c5ce00dccc7898facede
Lorenzo Lando <llando@google.com>
2020-02-26 11:43:48 +0000

    first eviroment setting

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-body></app-body>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './componets/header.vue'
+import Body from './componets/body.vue'
+import Footer from './componets/footer.vue'
+export default {
+  components: {
+    'app-header': Header,
+    'app-body': Body,
+    'app-footer': Footer
+
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/componets/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <h1>{{title}}</h1>
+   <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Lorenzo Vueapp'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
